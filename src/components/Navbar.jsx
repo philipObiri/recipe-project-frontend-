6efb748179115595feffc58fc1eb837f662791c3
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-26 16:50:08 +0000

    Navigation bar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import {faHome, faList, faCog} from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    function closeSideBar(){
+        setShowSidebar(false);
+    }
+
+    const links = [
+        {
+            name: "Home",
+            path: "/",
+            icon:faHome
+        },
+        {
+            name: "Recipes",
+            path: "/recipes",
+            icon:faList
+        },
+        {
+            name: "Settings",
+            path: '/settings',
+            icon:faCog
+        }
+    ];
+
+
+    return (
+        <>
+            <div className="navbar container">
+                <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+                <div className="nav-links">
+                    { links.map((link)=>(
+                        <a href="#!" key={link.name}> {link.name}</a>
+                    ))}
+                </div>
+
+
+                <div onClick={() => setShowSidebar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+            </div>
+
+           {showSidebar && <Sidebar  close={closeSideBar} links={links} />}
+        </>
+    )
+}
+
+export default Navbar;
