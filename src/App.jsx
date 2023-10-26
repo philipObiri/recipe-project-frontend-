d7c059a55a70ba7374f79b672cea08cf81bf991a
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-26 16:49:37 +0000

    task: Set up project navigation bar and sidebar components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <>
+    <nav>
+    <Navbar/>
+    </nav>
+    </>
+  )
+}
+
+export default App;
