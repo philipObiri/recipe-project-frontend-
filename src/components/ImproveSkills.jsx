b438af5f839aef115dbf70574cf6a4ac94dc0311
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-27 22:47:17 +0000

    task: Fixed spelling error on the signup button

--- src/components/ImproveSkills.jsx
+++ src/components/ImproveSkills.jsx
@@ -18,7 +18,7 @@ export default function ImproveSkills(){
                 { list.map((item, index) => (
                     <p className="skill-item" key={index}>{item}</p>
                 )) }
-                <button className="btn">singup now</button>
+                <button className="btn">SignUp Now</button>
             </div>
         </div>
     )
