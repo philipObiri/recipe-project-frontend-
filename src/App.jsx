0cfbd897a63701f5ca5f15a7a548989649f15a2c
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-27 22:40:08 +0000

    Import and layout components in the projects main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-// import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Settings from "./pages/Settings";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-    <nav>
-    <Navbar/>
-    </nav>
-    </>
+    <Router>
+      <Navbar />
+      <main className="container main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
