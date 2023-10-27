72fd9d6d9b09e639762ceba5e823ad5188483a5e
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-27 22:37:15 +0000

    Navgationbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,50 @@
-import { useState, useEffect } from "react";
-import Sidebar from "./Sidebar";
-import {faHome, faList, faCog} from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation } from "react-router-dom"
 
-function Navbar() {
-    const [showSidebar, setShowSidebar] = useState(false);
+import { useState } from "react"
+import Sidebar from "./Sidebar"
 
-    function closeSideBar(){
-        setShowSidebar(false);
-    }
+import { faHome, faList, faCog } from "@fortawesome/free-solid-svg-icons"
 
+export default function Navbar(){
+    const [showSidebar, setShowSidebar] = useState(false)
+    const location = useLocation()
     const links = [
         {
             name: "Home",
             path: "/",
-            icon:faHome
+            icon: faHome
         },
         {
             name: "Recipes",
             path: "/recipes",
-            icon:faList
+            icon: faList
         },
         {
             name: "Settings",
-            path: '/settings',
-            icon:faCog
+            path: "/settings",
+            icon: faCog
         }
-    ];
+    ]
 
-
+    function closeSidebar(){
+        setShowSidebar(false)
+    }
     return (
         <>
-            <nav className="navbar container">
-                <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+            <div className="navbar container">
+                <Link to="/" className="logo">F<span>oo</span>diesHub</Link>
                 <div className="nav-links">
-                    { links.map((link)=>(
-                        <a href="#!" key={link.name}> {link.name}</a>
-                    ))}
+                    { links.map(link => (
+                        <Link className={location.pathname === link.path ? "active" : ""} to={link.path} key={link.name}>{link.name}</Link>
+                    )) }
                 </div>
-
-
                 <div onClick={() => setShowSidebar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
                     <div className="bar"></div>
                     <div className="bar"></div>
                     <div className="bar"></div>
                 </div>
-            </nav>
-
-           {showSidebar && <Sidebar  close={closeSideBar} links={links} />}
+            </div>
+            { showSidebar && <Sidebar close={closeSidebar} links={links} /> }
         </>
     )
 }
-
-export default Navbar;
